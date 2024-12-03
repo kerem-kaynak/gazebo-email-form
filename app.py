60dcf8349ebffb627bdecbb3839ce02024a632dd
Kerem Kaynak <keremakaynak@gmail.com>
2024-12-03 23:49:40 +0100

    add cors

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("EMAIL_ADDRESS")
 app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASSWORD")
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("EMAIL_ADDRESS")
+app.config["MAIL_DEFAULT_SENDER"] = ("Gazebo İletişim", os.getenv("EMAIL_ADDRESS"))
 
 mail = Mail(app)
 
